ffabff714d8328024c4c12f40ccaa706f12ae80c
David Tuta <santiagotutadiaz@gmail.com>
2023-06-01 11:53:34 -0500

    d

---
+++ src/app/admin/pages/informes/informes-form/informes-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {Chart,registerables} from 'node_modules/chart.js';
+
+@Component({
+  selector: 'app-informes-form',
+  templateUrl: './informes-form.component.html',
+  styleUrls: ['./informes-form.component.scss']
+})
+export class InformesFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
