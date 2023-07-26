b1b680f03b54744eaf24fee9e83bd9c4406d751a
David Tuta <santiagotutadiaz@gmail.com>
2023-07-26 09:04:06 -0500

    d

--- src/app/admin/pages/IndividualesObjectives/feeback-actions/feeback-actions.component.ts
+++ src/app/admin/pages/IndividualesObjectives/feeback-actions/feeback-actions.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatAccordion } from '@angular/material/expansion';
 import { FeedbackActionsService } from 'src/app/admin/services/feedback-actions.service';
 import { LocalService } from 'src/app/config/local.service';
 import { SnackbarService } from 'src/app/config/snackbar.service';
@@ -9,10 +10,15 @@ import { SnackbarService } from 'src/app/config/snackbar.service';
   styleUrls: ['./feeback-actions.component.scss']
 })
 export class FeebackActionsComponent implements OnInit {
-  title?: string;
-  listFeedback?: any = [];
+
+  @ViewChild(MatAccordion) accordion?: MatAccordion;
+  title: string = '';
+  listFeedback: any[] = [];
   validateTime: any;
-validateTime2: any;
+  validateTime2: any;
+  titleButton: string = 'Crear';
+  showList: boolean = false;
+   showForm: boolean = false;
 
   constructor(
     private FeebackAPI: FeedbackActionsService,
@@ -21,59 +27,96 @@ validateTime2: any;
   ) { }
 
   ngOnInit(): void {
-     this.title = '';
+    this.title = '';
   }
 
-  changeUpdate(index: number, type: string){
+
+
+
+
+  loadFeedbacks() {
+    this.FeebackAPI.FindAll({}).then((response: any) => {
+      this.listFeedback = response; // Asignar la respuesta al array listFeedback
+    }).catch((error) => {
+      // Manejar errores si es necesario
+    });
+  }
+
+  changeUpdate(index: number, type: string) {
     clearTimeout(this.validateTime2);
     this.validateTime2 = setTimeout(() => {
-      if(type == 'title' && !this.listFeedback[index].update && this.listFeedback[index].sync){
+      if (type == 'title' && !this.listFeedback[index].update && this.listFeedback[index].sync) {
         this.listFeedback[index].update = true;
       }
     }, 1500);
   }
 
-  addFeedback(){
-    if(!this.validateFeedback()){
+
+  addFeedback() {
+    this.showForm = true;
+    if (!this.validateFeedback()) {
       return;
     }
 
-    this.listFeedback.unshift({
-      id: null,
-      unique_id: null,
+    this.listFeedback.push({
       title: '',
       update: false,
       create: true,
       sync: false,
       delete: false
     });
+
+    this.showList = true;
+
   }
 
-  validateFeedback(){
-    let result = true;
+  deleteFeedback(index: number) {
+    // Lógica para eliminar un elemento de la listas
+    this.listFeedback.splice(index, 1);
 
-    for (let i = 0; i < this.listFeedback.length; i++) {
-      if(!this.listFeedback[i].title){
-        this.snack.viewsnack('Hace falta la descripción de la accion de retroalimentacion', 'error');
-        result = false;
-      }
-    }
-
-    return result;
   }
 
-  async saveData() {
-    if(!this.title){
-      return this.snack.viewsnack('Hace Falta el Titulo', 'ERROR');
-    }
 
-    const data = {
-      title: this.title,
+ validateFeedback() {
+  for (let i = 0; i < this.listFeedback.length; i++) {
+    if (!this.listFeedback[i].title) {
+      this.snack.viewsnack('Hace falta la descripción de la acción de retroalimentación', 'error');
+      return false;
     }
-
-    this.FeebackAPI.Create(data).then((res: any) => {
-      this.snack.viewsnack(res.data.msg, 'success');
-    });
   }
+  return true;
+}
+
+
+async saveData() {
+  if (!this.title) {
+    return this.snack.viewsnack('Hace Falta el Titulo', 'ERROR');
+  }
+
+  const data = {
+    title: this.title
+  };
+
+  try {
+    const response = await this.FeebackAPI.Create(data);
+    if (response.res && response.data) {
+      // Mostrar un mensaje de éxito
+      this.snack.viewsnack(response.data.msg, 'success');
+
+      // Actualizar la lista de retroalimentación desde el backend
+      await this.loadFeedbacks();
+    } else {
+      // Mostrar un mensaje de error si la respuesta no es la esperada
+      this.snack.viewsnack('Hubo un error al crear la Accion de Retroalimentación', 'error');
+    }
+  } catch (error) {
+    // Manejar errores aquí
+    this.snack.viewsnack('Hubo un error al crear la Accion de Retroalimentación', 'error');
+  }
+
+  this.title = '';
+  this.showList = false;
+}
+
 
 }
--- src/app/admin/services/feedback-actions.service.ts
+++ src/app/admin/services/feedback-actions.service.ts
@@ -14,7 +14,40 @@ export class FeedbackActionsService {
   Create(body: any) {
     this.token = this.Local.findDataLocal('token');
     return api.post(`/feedback/create`, body, {
-      headers:{Authorization: "Bearer" + this.token}
+      headers:{ Authorization: "Bearer " + this.token }
+    })
+      .then((res) => res.data)
+      .catch((err) => {
+      throw err.response
+    })
+  }
+
+  FindAll(body: any) {
+    this.token = this.Local.findDataLocal('token');
+    return api.post(`/feedback/getAll`, body, {
+      headers:{Authorization: "Bearer " + this.token}
+    })
+    .then((res) => res.data)
+      .catch((err) => {
+      throw err.response
+    })
+  }
+
+  Update(uuid: string, body: any) {
+    this.token = this.Local.findDataLocal('token');
+    return api.put(`/feedback/update/${uuid}`, body, {
+      headers: {Authorization: "Bearer " + this.token}
+    })
+      .then((res) => res.data)
+      .catch((err) => {
+      throw err.response
+    })
+  }
+
+  Delete(uuid: string) {
+    this.token = this.Local.findDataLocal('token');
+    return api.delete(`/feedback/delete/${uuid}`, {
+      headers: {Authorization: "Bearer " + this.token}
     })
       .then((res) => res.data)
       .catch((err) => {
