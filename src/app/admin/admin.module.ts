73d9d2d2717401fca3e0cf4a417170b52fde3161
David Tuta <santiagotutadiaz@gmail.com>
2023-11-01 10:56:26 -0500

    d

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -10,10 +10,6 @@ import { MatPaginatorIntl } from '@angular/material/paginator';
 import {MatTableModule} from '@angular/material/table';
 import { CustomPaginator } from '../config/matPaginador';
 import { ScrollingModule } from '@angular/cdk/scrolling';
-import { InformesChart1Component } from './pages/informes/informes-chart1/informes-chart1.component';
-import { InformesChart2Component } from './pages/informes/informes-chart2/informes-chart2.component';
-import { InformesChart3Component } from './pages/informes/informes-chart3/informes-chart3.component';
-import { InformesChart4Component } from './pages/informes/informes-chart4/informes-chart4.component';
 
 
 
@@ -22,10 +18,6 @@ import { InformesChart4Component } from './pages/informes/informes-chart4/inform
   declarations: [
     ...fromContainers.AdminComponents,
     ...fromContainers.AdminPages,
-    InformesChart1Component,
-    InformesChart2Component,
-    InformesChart3Component,
-    InformesChart4Component,
   ],
   imports: [
     CommonModule,
